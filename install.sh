a196d0e53b9b69ad37cd23c2f450b7c03ba6b350
Sinclair Hudson <30478652+SinclairHudson@users.noreply.github.com>
2020-10-06 13:56:58 -0400

    added plug install to install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ apt-get install curl
 apt-get install nodejs
 chsh -s $(which zsh)
 
+mkdir ~/.vim/undodir -p
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
