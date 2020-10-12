a7ccdd7108b6824a056c09d84cc509c38bf27909
Sinclair Hudson <30478652+SinclairHudson@users.noreply.github.com>
2020-10-12 17:30:54 -0400

    Update install.sh
    Making nvim install

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ apt-get install neovim
 chsh -s $(which zsh)
 
 mkdir ~/.vim/undodir -p # make the undo directory for vim
+mkdir ~/.config/nvim -p
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
